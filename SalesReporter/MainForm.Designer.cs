ec98429a1020bf5884eb6ccbd28ae1f45640ed6c
mon_cristo <mon_cristo@outlook.com>
2018-08-28 13:22:43 +0300

    Reports app creation

---
+++ SalesReporter/MainForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace SalesReporter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInputBrowse = new System.Windows.Forms.Button();
+            this.btnOutputBrowse = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnInputBrowse
+            // 
+            this.btnInputBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInputBrowse.Location = new System.Drawing.Point(488, 57);
+            this.btnInputBrowse.Name = "btnInputBrowse";
+            this.btnInputBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnInputBrowse.TabIndex = 0;
+            this.btnInputBrowse.Text = "Browse...";
+            this.btnInputBrowse.UseVisualStyleBackColor = true;
+            this.btnInputBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnOutputBrowse
+            // 
+            this.btnOutputBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOutputBrowse.Location = new System.Drawing.Point(488, 131);
+            this.btnOutputBrowse.Name = "btnOutputBrowse";
+            this.btnOutputBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnOutputBrowse.TabIndex = 1;
+            this.btnOutputBrowse.Text = "Browse...";
+            this.btnOutputBrowse.UseVisualStyleBackColor = true;
+            this.btnOutputBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(113, 59);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(369, 20);
+            this.txtInput.TabIndex = 2;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(113, 133);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(369, 20);
+            this.txtOutput.TabIndex = 3;
+            this.txtOutput.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Location = new System.Drawing.Point(488, 195);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "Create";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip.Location = new System.Drawing.Point(0, 239);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(584, 22);
+            this.statusStrip.TabIndex = 5;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(39, 17);
+            this.lblStatus.Text = "Ready";
+            // 
+            // lblInput
+            // 
+            this.lblInput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.Location = new System.Drawing.Point(28, 60);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(71, 15);
+            this.lblInput.TabIndex = 6;
+            this.lblInput.Text = "Input folder:";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.lblOutput.Location = new System.Drawing.Point(28, 136);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(80, 15);
+            this.lblOutput.TabIndex = 7;
+            this.lblOutput.Text = "Output folder:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnOutputBrowse);
+            this.Controls.Add(this.btnInputBrowse);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 400);
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Reporter";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnInputBrowse;
+        private System.Windows.Forms.Button btnOutputBrowse;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ SalesReporter/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesReporter
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            SetDefultStatus("Please, select input folder...");
+        }
+
+        private void SetDefultStatus(string msg = null, bool isRed = true)
+        {
+            lblStatus.Text = msg ?? "Ready to create report...";
+            lblStatus.ForeColor = (isRed) ? Color.Red : Color.Green;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            bool isInput = ((Button)sender) == btnInputBrowse;
+
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+
+            folderDlg.ShowNewFolderButton = true;
+
+            folderDlg.SelectedPath = isInput ? txtInput.Text : txtOutput.Text;
+
+            DialogResult result = folderDlg.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                if (isInput)
+                {
+                    txtInput.Text = folderDlg.SelectedPath;
+                    SetDefultStatus("Please, select output folder...");
+                    btnOutputBrowse.Focus();
+                }
+                else
+                {
+                    txtOutput.Text = folderDlg.SelectedPath;
+                    SetDefultStatus(null, false);
+                    btnOK.Focus();
+                }
+            }
+
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
